69b6609aa1b7d18973f12737f57331fdb26610a7
DerekFurstPitt <drf57@pitt.edu>
2023-04-27 14:03:39 -0400

    Completed sorting and finalized columns to appear in the table.

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -5,6 +5,7 @@ import {Table} from "antd";
 const DataTable = () => {
     const [gridData, setGridData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [sortedInfo, setSortedInfo] = useState({});
 
     useEffect(() => {
         loadData();
@@ -34,120 +35,133 @@ const DataTable = () => {
             title: "HuBMAP ID",
             dataIndex: "hubmap_id",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.hubmap_id.localeCompare(b.hubmap_id)
         },
         {
             title: "Group Name",
             dataIndex: "group_name",
             align: "center",
             editTable: true,
+            sorter: (a,b) => a.group_name.localeCompare(b.group_name)
         },
         {
             title: "Status",
             dataIndex: "status",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.status.localeCompare(b.status)
         },
         {
             title: "Organ Type",
             dataIndex: "organ",
             align: "center",
             editTable: true,
+            sorter: (a,b) => a.organ.localeCompare(b.organ)
         },
         {
             title: "Provider Experiment ID",
             dataIndex: "provider_experiment_id",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.provider_experiment_id.localeCompare(b.provider_experiment_id)
         },
         {
             title: "Last Touch",
             dataIndex: "last_touch",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => new Date(a.last_touch) - new Date(b.last_touch)
         },
         {
             title: "Has Contacts",
             dataIndex: "has_contacts",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => b.has_contacts.localeCompare(a.has_contacts)
         },
         {
             title: "Has Contributors",
             dataIndex: "has_contributors",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => b.has_contributors.localeCompare(a.has_contributors)
         },
         {
             title: "Data Types",
             dataIndex: "datatypes",
             align: "center",
-            editTable: true
-        },
-        {
-            title: "Donor UUID",
-            dataIndex: "donor_uuid",
-            align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.datatypes.localeCompare(b.datatypes)
         },
         {
             title: "Donor HuBMAP ID",
             dataIndex: "donor_hubmap_id",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.donor_hubmap_id.localeCompare(b.donor_hubmap_id)
         },{
             title: "Donor Submission ID",
             dataIndex: "donor_submission_id",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.donor_submission_id.localeCompare(b.donor_submission_id)
         },
         {
             title: "Donor Lab ID",
             dataIndex: "donor_lab_id",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.donor_lab_id.localeCompare(b.donor_lab_id)
         },
         {
             title: "Has Donor Metadata",
             dataIndex: "has_metadata",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => b.has_metadata.localeCompare(a.has_metadata)
         },
         {
             title: "Parent Dataset",
             dataIndex: "parent_dataset",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.parent_dataset.localeCompare(b.parent_dataset)
         },
         {
             title: "Upload",
             dataIndex: "upload",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.upload.localeCompare(b.upload)
         },
         {
             title: "Has Rui Info",
             dataIndex: "has_rui_info",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => b.has_rui_info.localeCompare(a.has_rui_info)
         },
         {
             title: "Globus URL",
             dataIndex: "globus_url",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url)
         },
         {
             title: "Portal URL",
             dataIndex: "portal_url",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url)
         },
         {
             title: "Ingest URL",
             dataIndex: "ingest_url",
             align: "center",
-            editTable: true
+            editTable: true,
+            sorter: (a,b) => a.ingest_url.localeCompare(b.ingest_url)
         }
     ]
 
