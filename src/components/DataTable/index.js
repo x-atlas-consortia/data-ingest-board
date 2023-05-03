cf24bdd0cfeebda4869db823b0d70621937a3649
DerekFurstPitt <drf57@pitt.edu>
2023-05-03 17:35:05 -0400

    Filters for columns have been implemented. Including filters for datasets associated with a given upload id now. work in progress

--- src/components/DataTable/index.js
+++ src/components/DataTable/index.js
@@ -3,138 +3,171 @@ import axios from "axios";
 import { Table } from "antd";
 
 const DatasetTable = ({ data, loading }) => {
+    const uniqueGroupNames = [...new Set(data.map(item => item.group_name))];
+    const unfilteredOrganTypes = [...new Set(data.map(item => item.organ))];
+    const uniqueOrganType = unfilteredOrganTypes.filter(name => name !== "" && name !== " ");
+    const uniqueDataType = [...new Set(data.map(item => item.data_types))]
     const datasetColumns = [
         {
             title: "HuBMAP ID",
             dataIndex: "hubmap_id",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.hubmap_id.localeCompare(b.hubmap_id)
+            sorter: (a,b) => a.hubmap_id.localeCompare(b.hubmap_id),
         },
         {
             title: "Group Name",
             dataIndex: "group_name",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.group_name.localeCompare(b.group_name)
+            sorter: (a,b) => a.group_name.localeCompare(b.group_name),
+            filters: uniqueGroupNames.map(name => ({ text: name, value: name })),
+            onFilter: (value, record) => record.group_name === value,
         },
         {
             title: "Status",
             dataIndex: "status",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.status.localeCompare(b.status)
+            sorter: (a,b) => a.status.localeCompare(b.status),
+            filters: [
+                {text: 'Unpublished', value: 'Unpublished'},
+                {text: 'Published', value: 'Published'},
+                {text: 'QA', value: 'QA'},
+                {text: 'Error', value: 'Error'},
+                {text: 'Invalid', value: 'Invalid'},
+                {text: 'New', value: 'New'},
+                {text: 'Processing', value: 'Processing'},
+                {text: 'Submitted', value: 'Submitted'}
+            ],
+            onFilter: (value, record) => {
+                if (value === 'Unpublished') {
+                    return record.status !== 'Published';
+                }
+                return record.status === value;
+            }
         },
         {
             title: "Organ Type",
             dataIndex: "organ",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.organ.localeCompare(b.organ)
-        },
-        {
-            title: "Provider Experiment ID",
-            dataIndex: "provider_experiment_id",
-            align: "center",
-            editTable: true,
-            sorter: (a,b) => a.provider_experiment_id.localeCompare(b.provider_experiment_id)
-        },
-        {
-            title: "Last Touch",
-            dataIndex: "last_touch",
-            align: "center",
-            editTable: true,
-            sorter: (a,b) => new Date(a.last_touch) - new Date(b.last_touch)
-        },
-        {
-            title: "Has Contacts",
-            dataIndex: "has_contacts",
-            align: "center",
-            editTable: true,
-            sorter: (a,b) => b.has_contacts.localeCompare(a.has_contacts)
-        },
-        {
-            title: "Has Contributors",
-            dataIndex: "has_contributors",
-            align: "center",
-            editTable: true,
-            sorter: (a,b) => b.has_contributors.localeCompare(a.has_contributors)
+            sorter: (a,b) => a.organ.localeCompare(b.organ),
+            filters: uniqueOrganType.map(name => ({ text: name, value: name })),
+            onFilter: (value, record) => record.organ === value,
         },
         {
             title: "Data Types",
             dataIndex: "data_types",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.data_types.localeCompare(b.data_types)
+            sorter: (a,b) => a.data_types.localeCompare(b.data_types),
+            filters: uniqueDataType.map(name => ({ text: name, value: name })),
+            onFilter: (value, record) => record.data_types === value,
+        },
+        {
+            title: "Provider Experiment ID",
+            dataIndex: "provider_experiment_id",
+            align: "center",
+            editTable: true,
+            sorter: (a,b) => a.provider_experiment_id.localeCompare(b.provider_experiment_id),
+        },
+        {
+            title: "Last Touch",
+            dataIndex: "last_touch",
+            align: "center",
+            editTable: true,
+            sorter: (a,b) => new Date(a.last_touch) - new Date(b.last_touch),
+        },
+        {
+            title: "Has Contacts",
+            dataIndex: "has_contacts",
+            align: "center",
+            editTable: true,
+            sorter: (a,b) => b.has_contacts.localeCompare(a.has_contacts),
+        },
+        {
+            title: "Has Contributors",
+            dataIndex: "has_contributors",
+            align: "center",
+            editTable: true,
+            sorter: (a,b) => b.has_contributors.localeCompare(a.has_contributors),
         },
         {
             title: "Donor HuBMAP ID",
             dataIndex: "donor_hubmap_id",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.donor_hubmap_id.localeCompare(b.donor_hubmap_id)
+            sorter: (a,b) => a.donor_hubmap_id.localeCompare(b.donor_hubmap_id),
         },{
             title: "Donor Submission ID",
             dataIndex: "donor_submission_id",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.donor_submission_id.localeCompare(b.donor_submission_id)
+            sorter: (a,b) => a.donor_submission_id.localeCompare(b.donor_submission_id),
         },
         {
             title: "Donor Lab ID",
             dataIndex: "donor_lab_id",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.donor_lab_id.localeCompare(b.donor_lab_id)
+            sorter: (a,b) => a.donor_lab_id.localeCompare(b.donor_lab_id),
         },
         {
             title: "Has Donor Metadata",
             dataIndex: "has_metadata",
             align: "center",
             editTable: true,
-            sorter: (a,b) => b.has_metadata.localeCompare(a.has_metadata)
+            sorter: (a,b) => b.has_metadata.localeCompare(a.has_metadata),
         },
         {
             title: "Parent Dataset",
             dataIndex: "parent_dataset",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.parent_dataset.localeCompare(b.parent_dataset)
+            sorter: (a,b) => a.parent_dataset.localeCompare(b.parent_dataset),
         },
         {
             title: "Upload",
             dataIndex: "upload",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.upload.localeCompare(b.upload)
+            sorter: (a,b) => a.upload.localeCompare(b.upload),
         },
         {
             title: "Has Rui Info",
             dataIndex: "has_rui_info",
             align: "center",
             editTable: true,
-            sorter: (a,b) => b.has_rui_info.localeCompare(a.has_rui_info)
+            sorter: (a,b) => b.has_rui_info.localeCompare(a.has_rui_info),
+        },
+        {
+            title: "Has Data",
+            dataIndex: "has_data",
+            align: "center",
+            editTable: true,
+            sorter: (a,b) => b.has_data.localeCompare(a.has_data),
         },
         {
             title: "Globus URL",
             dataIndex: "globus_url",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url)
+            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url),
         },
         {
             title: "Portal URL",
             dataIndex: "portal_url",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url)
+            sorter: (a,b) => a.globus_url.localeCompare(b.globus_url),
         },
         {
             title: "Ingest URL",
             dataIndex: "ingest_url",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.ingest_url.localeCompare(b.ingest_url)
+            sorter: (a,b) => a.ingest_url.localeCompare(b.ingest_url),
         }
     ]
 
@@ -152,54 +185,55 @@ const DatasetTable = ({ data, loading }) => {
 
 const UploadTable = ({ data, loading }) => {
     const uploadColumns = [
+        // {
+        //     title: "Datasets",
+        //     dataIndex: "datasets",
+        //     align: "center",
+        //     editTable: true,
+        //     sorter: (a,b) => a.datasets.localeCompare(b.datasets)
+        // },
         {
-            title: "Datasets",
-            dataIndex: "datasets",
+            title: "HuBMAP ID",
+            dataIndex: "hubmap_id",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.datasets.localeCompare(b.datasets)
+            sorter: (a,b) => a.hubmap_id.localeCompare(b.hubmap_id),
+            filter: true
         },
         {
             title: "Group Name",
             dataIndex: "group_name",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.group_name.localeCompare(b.group_name)
-        },
-        {
-            title: "HuBMAP ID",
-            dataIndex: "hubmap_id",
-            align: "center",
-            editTable: true,
-            sorter: (a,b) => a.hubmap_id.localeCompare(b.hubmap_id)
+            sorter: (a,b) => a.group_name.localeCompare(b.group_name),
         },
         {
             title: "Ingest Url",
             dataIndex: "ingest_url",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.ingest_url.localeCompare(b.ingest_url)
+            sorter: (a,b) => a.ingest_url.localeCompare(b.ingest_url),
         },
         {
             title: "Status",
             dataIndex: "status",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.status.localeCompare(b.status)
+            sorter: (a,b) => a.status.localeCompare(b.status),
         },
         {
             title: "Title",
             dataIndex: "title",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.title.localeCompare(b.title)
+            sorter: (a,b) => a.title.localeCompare(b.title),
         },
         {
             title: "UUID",
             dataIndex: "uuid",
             align: "center",
             editTable: true,
-            sorter: (a,b) => a.uuid.localeCompare(b.uuid)
+            sorter: (a,b) => a.uuid.localeCompare(b.uuid),
         }
     ];
 
@@ -215,18 +249,33 @@ const UploadTable = ({ data, loading }) => {
     );
 };
 
-const DataTable = () => {
+const DataTable = (props) => {
     const [datasetData, setDatasetData] = useState([]);
     const [uploadData, setUploadData] = useState([]);
+    const [datasetsInUploadData, setDatasetsInUploadData] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [useDatasetApi, setUseDatasetApi] = useState(true);
+    const [useDatasetApi, setUseDatasetApi] = useState(props.useDatasetApi);
+    const [selectUploadId, setSelectUploadId] = useState(props.selectUploadId);
+    const [invalidUploadId, setInvalidUploadId] = useState(false);
     const datasetUrl = "http://localhost:8484/datasets/data-status";
     const uploadUrl = "http://localhost:8484/uploads/data-status";
-
+    console.log(`upload id is ${selectUploadId}!`);
     useEffect(() => {
         loadData();
     }, []);
 
+    if (typeof selectUploadId !== 'undefined') {
+        const matchingUpload = uploadData.find(obj => obj.uuid === selectUploadId || obj.hubmap_id === selectUploadId);
+        if (typeof matchingUpload === 'undefined') {
+            setInvalidUploadId(true);
+        }
+        else {
+            setUseDatasetApi(true);
+            const listOfDatasets = matchingUpload.split(',');
+
+        }
+
+    }
     const loadData = async () => {
         setLoading(true);
         try {
@@ -263,6 +312,7 @@ const DataTable = () => {
             <center>
                 <h2>{useDatasetApi ? "Datasets" : "Uploads"}</h2>
             </center>
+            {invalidUploadId && <p style={{ color: "red" }}>Upload ID Not Found</p>}
             <button onClick={toggleApi}>
                 {useDatasetApi ? "Switch to Uploads Table" : 'Switch to Datasets Table'}
             </button>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,24 @@
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
 import DataTable from "@/components/DataTable";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App" style={{padding: '0 80px'}}>
-        <center>
-            <h1>Dataset Publication Dashboard</h1>
-        </center>
-        <DataTable />
-    </div>
-  )
+function App({ entity_type, upload_id }) {
+    const useDatasetApi = entity_type !== 'uploads';
+    const [selectUploadId, setSelectUploadId] = useState(upload_id);
+    return (
+        <div className="App" style={{ padding: "0 80px" }}>
+            <center>
+                <h1>Dataset Publication Dashboard</h1>
+            </center>
+            <DataTable selectUploadId={selectUploadId} setSelectUploadId={setSelectUploadId} useDatasetApi={useDatasetApi} />
+        </div>
+    );
 }
+
+App.getInitialProps = ({ query }) => {
+  const { entity_type, upload_id } = query;
+  return { entity_type, upload_id };
+};
 
 export default App;
