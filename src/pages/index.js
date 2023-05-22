6a9c0b66df77a676bc8fe7f349d473125d35aa5a
DerekFurstPitt <drf57@pitt.edu>
2023-05-22 16:08:09 -0400

    begun adding css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 // import styles from '@/styles/Home.module.css'
+import styles from '@/styles/index.module.css'
 import DataTable from "@/components/DataTable";
 import { useState } from "react";
 
 function App({ entity_type, upload_id, page, page_size, sort_field, sort_order, filters }) {
-    //const useDatasetApi = entity_type !== 'uploads';
     const [entityType, setEntityType] = useState(entity_type);
     const [selectUploadId, setSelectUploadId] = useState(upload_id);
     const [initialPage, setInitialPage] = useState(page);
@@ -16,9 +16,14 @@ function App({ entity_type, upload_id, page, page_size, sort_field, sort_order, 
     const [tableFilters, setTableFilters] = useState(filters);
     return (
         <div className="App" style={{ padding: "0 80px" }}>
-            <center>
-                <h1>Dataset Publication Dashboard</h1>
-            </center>
+            <div className={styles.Banner}>
+                <h1 className={styles.Logo}>
+                    HuBMAP
+                </h1>
+                <h1 className={styles.Title}>
+                    Dataset Publication Dashboard
+                </h1>
+            </div>
             <DataTable
                 entityType={entityType}
                 setEntityType={setEntityType}
