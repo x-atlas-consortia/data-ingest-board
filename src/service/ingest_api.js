0c8bde22650f05c58e29def437a8c65fd77e6b44
Lisa-Ann B <lisabruney@pitt.edu>
2023-08-10 14:48:41 -0400

    Resolve for groups and user_write_groups

--- src/service/ingest_api.js
+++ src/service/ingest_api.js
@@ -15,7 +15,7 @@ export function ingest_api_users_groups(auth) {
     };
     return axios.get(URLS.ingest.privs.userGroups(),  options).then(res => {
         console.debug('API USER GROUPs', res.data.groups);
-        return {status: res.status, results: res.data.groups}
+        return {status: res.status, results: res.data.groups || res.data.user_write_groups}
    })
     .catch(error => {
         console.debug("ingest_api_allowable_edit_states", error, error.response);
