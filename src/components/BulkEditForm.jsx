bc04b06fae7fdda609c0512e0b48cee4e0a63d4b
Lisa-Ann B <lisabruney@pitt.edu>
2024-05-14 11:59:44 -0400

    Add ability to clear values

--- src/components/BulkEditForm.jsx
+++ src/components/BulkEditForm.jsx
@@ -1,6 +1,6 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-import {Form, Input, Select} from "antd";
+import {Form, Input, Select, Checkbox} from "antd";
 
 function BulkEditForm({statuses, writeGroups, setBulkEditValues}) {
     const [bulkEditForm] = Form.useForm()
@@ -29,10 +29,14 @@ function BulkEditForm({statuses, writeGroups, setBulkEditValues}) {
         updateBulk(field, value)
     }
 
+    const handleResetChecked = (field) => {
+        updateBulk(field, "")
+    }
+
     return (
         <>
             <Form form={bulkEditForm} name="validateOnly" layout="vertical" autoComplete="off">
-                <Form.Item name="assigned_to_group_name" label="Assigned To Group Name">
+                <Form.Item name="assigned_to_group_name" label="Assigned To Group Name" className={'mb-0'}>
                     <Select
                         placeholder="Select a group to assign the task to"
                         allowClear
@@ -41,10 +45,14 @@ function BulkEditForm({statuses, writeGroups, setBulkEditValues}) {
                         {groupOptions}
                     </Select>
                 </Form.Item>
-                <Form.Item name="ingest_task" label="Ingest Task">
+                <Checkbox onChange={()=>handleResetChecked('assigned_to_group_name')}>Clear assigned to group name values on selected </Checkbox>
+
+                <Form.Item name="ingest_task" label="Ingest Task" className={'mt-3 mb-0'}>
                     <Input />
                 </Form.Item>
-                <Form.Item name="status" label="Status">
+                <Checkbox onChange={()=>handleResetChecked('ingest_task')}>Clear ingest task values on selected </Checkbox>
+
+                <Form.Item name="status" label="Status" className={'mt-3'}>
                     <Select
                         placeholder="Select a new dataset status"
                         onChange={handleOptionsChange}
