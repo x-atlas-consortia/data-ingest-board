c14948d0a79acc4b8e2c4f04891329cea778e40f
Tyler Madonna <tjm159@pitt.edu>
2024-08-19 16:25:42 -0400

    Add '@' path alias for cleaner imports

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,24 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-   transpilePackages: [ 'antd', '@ant-design', 'rc-util', 'rc-tree', 'rc-table', 'rc-pagination', 'rc-picker', 'rc-notification', 'rc-tooltip' ]
+    reactStrictMode: true,
+    webpack: (config, options) => {
+        // Set the @ alias for the src directory
+        config.resolve.alias['@'] = path.resolve(__dirname)
+        return config
+    },
+    transpilePackages: [
+        'antd',
+        '@ant-design',
+        'rc-util',
+        'rc-tree',
+        'rc-table',
+        'rc-pagination',
+        'rc-picker',
+        'rc-notification',
+        'rc-tooltip'
+    ]
 }
 
 module.exports = nextConfig
