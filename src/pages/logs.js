69727af9bf75580d5ce560057b5f4ee8ca378fed
Lisa-Ann B <lisabruney@pitt.edu>
2025-09-02 16:07:48 -0400

    Update dashboard with search api connection - #356

--- src/pages/logs.js
+++ src/pages/logs.js
@@ -1,6 +1,10 @@
-import React, {useContext, useState} from 'react';
-import {Col, DatePicker, Layout, Row, theme} from 'antd';
+import React, {useContext, useEffect, useState} from 'react';
+import {Card, Col, DatePicker, Layout, Row, theme} from 'antd';
 import AppSideNavBar from "@/components/AppSideNavBar";
+import ENS from "@/lib/helpers/envs";
+import {callService, eq, getHeadersWith} from "@/lib/helpers/general";
+import ENVS from "@/lib/helpers/envs";
+import AppContext from "@/context/AppContext";
 const { Header, Sider, Content } = Layout;
 const { RangePicker } = DatePicker;
 const Logs = () => {
@@ -9,13 +13,63 @@ const Logs = () => {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
 
-    const {fromDate, setFromDate} = useState()
-    const {toDate, setToDate} = useState()
+    const {globusToken, hasDataAdminPrivs} = useContext(AppContext)
+
+    const [fromDate, setFromDate] = useState()
+    const [toDate, setToDate] = useState()
+    const [data, setData] = useState(null)
+    const [cards, setCards] = useState(null)
 
     const handleDateRange = (dates, dateStrings) => {
         console.log(dates, dateStrings)
         // dates: [dayjs, dayjs], dateStrings: [string, string]
     }
+
+    const getCardDetail = (key, data) => {
+        if (eq(key, 'openSourceRepos')) {
+            return <></>
+        }
+    }
+
+    const getCards = (data)=> {
+        let _cards = {
+            openSourceRepos: {
+                title: 'Open Source Repositories'
+            }
+        }
+        const sections = ENVS.logsIndicies() || {}
+        let comps = []
+        for (let s in sections) {
+            comps.push(<Card title={_cards[s].title} key={s} variant="borderless" style={{ width: 300 }}>
+               Content
+            </Card>)
+        }
+        setCards(comps)
+    }
+
+    const fetchData = async () => {
+        const sections = ENVS.logsIndicies() || {}
+        let _data = {}
+
+        for (let s in sections) {
+            let indicies = sections[s]
+            for (let i of indicies) {
+                if (!_data[i]) {
+                    _data[i] = await callService(ENVS.urlFormat.search(`/${i}/search`), getHeadersWith(globusToken).headers, {}, 'POST')
+                }
+
+            }
+        }
+        return data
+    }
+
+    useEffect(() => {
+        if (globusToken) {
+            fetchData().then((data)=>{
+                getCards(data)
+            })
+        }
+    }, [globusToken]);
 
     return (
         <Layout style={{ minHeight: '100vh' }}>
@@ -43,7 +97,7 @@ const Logs = () => {
                         borderRadius: borderRadiusLG,
                     }}
                 >
-                    Content
+                    {cards}
                 </Content>
             </Layout>
         </Layout>
