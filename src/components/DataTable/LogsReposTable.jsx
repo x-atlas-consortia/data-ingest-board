ce47a9749b06ffd6ae00eabb51de6090ba47b5c9
Lisa-Ann B <lisabruney@pitt.edu>
2025-09-16 16:07:45 -0400

    Update label

--- src/components/DataTable/LogsReposTable.jsx
+++ src/components/DataTable/LogsReposTable.jsx
@@ -229,7 +229,7 @@ const LogsReposTable = ({ }) => {
         },
     };
 
-    const yAxis = { label: "↑ Views" }
+    const yAxis = { label: "↑ Views/Clones" }
 
     return (<>
         {vizData.stackedBar?.length > 0 && <StackedBarWithLegend yAxis={yAxis} xAxis={{ formatter: formatNum }} data={vizData.stackedBar} subGroupLabels={subgroupLabels.current} chartId={'repos'} />}
