03dd27b4de013f87389115279c99fc8fdca66de5
Lisa-Ann B <lisabruney@pitt.edu>
2025-10-23 10:56:11 -0400

    Add chart hotlinking - #380

--- src/lib/helpers/general.js
+++ src/lib/helpers/general.js
@@ -65,29 +65,37 @@ Object.assign(Array.prototype, {
     }
 })
 
-String.prototype.toDashedCase = function() {
-  
-  return this
-    .replace(/[^a-zA-Z0-9]+/g, '-')
-    .toLowerCase();
-}
-
-String.prototype.format = function() {
-    let args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-        return typeof args[number] != 'undefined'
-            ? args[number]
-            : match
-            ;
-    });
-};
-
-String.prototype.upCaseFirst = function() {
-    if (this.length === 0) {
-        return "";
+Object.assign(String.prototype, {
+    toCamelCase() {
+        return this.replace(/\s(.)/g, function (a) {
+            return a.toUpperCase();
+        })
+        .replace(/\s/g, '')
+        .replace(/^(.)/, function (b) {
+            return b.toLowerCase();
+        });
+    },
+    toDashedCase() {
+        return this
+            .replace(/[^a-zA-Z0-9]+/g, '-')
+            .toLowerCase();
+    },
+    upCaseFirst() {
+        if (this.length === 0) {
+            return "";
+        }
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    format() {
+        let args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) {
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
     }
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
+})
 
 export const getUBKGName = (o) => {
     if (!window.UBKG) return o
