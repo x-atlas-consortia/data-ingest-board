1ebf0d13b0b0a35abbb1e3d4a081274c6236a792
Lisa-Ann B <lisabruney@pitt.edu>
2025-11-07 14:25:11 -0500

    Update ga logic

--- src/pages/usage/ga.js
+++ src/pages/usage/ga.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useRef, useEffect, useState, useContext } from 'react';
 import { Layout  } from 'antd';
 import AppSideNavBar from "@/components/AppSideNavBar";
 import AppContext from "@/context/AppContext";
@@ -7,27 +7,27 @@ import Unauthorized from '@/components/Unauthorized';
 import { eq } from '@/lib/helpers/general';
 import ENVS from '@/lib/helpers/envs';
 
-
 const UsageGoogleAnalytics = ({}) => {
-    const { globusToken, isLoading, isAuthenticated} = useContext(AppContext)
+    const { isAuthenticated} = useContext(AppContext)
     const [showUnauthorized, setShowUnauthorized] = useState(false)
     const [dataSource, setDataSource] = useState(null)
+    const lookerStudioFrame = useRef(null)
 
     const getGoogleLookerStudio = () => {
      
       const query = new URLSearchParams(window.location.search)
       const requestedDataSource = query.get('v')
       const lookerStudio = ENVS.lookerStudio()
-      let path = lookerStudio[0].path
+      let _dataSource = lookerStudio[0]
  
       for (let l of lookerStudio) {
           if (eq(requestedDataSource, l.name.toDashedCase())) {
-            path = l.path
+            _dataSource = l
             break
           }
       }
       
-      setDataSource(path)
+      setDataSource(_dataSource)
     }
 
     useEffect(() => {
@@ -45,7 +45,7 @@ const UsageGoogleAnalytics = ({}) => {
             <AppSideNavBar isGoogleAnalytics={true}  />
             {showUnauthorized && <div className='container mt-5'><Unauthorized withLayout={true} /></div>}
             {!showUnauthorized && dataSource && <Layout>
-                <iframe width="100%" height="100%" src={`https://lookerstudio.google.com/embed/reporting${dataSource}`} ></iframe>
+                <iframe ref={lookerStudioFrame} width="100%" height="100%" src={`https://lookerstudio.google.com/embed/reporting${dataSource.path}`} ></iframe>
             </Layout>}
         </Layout>
     );
